c6343e5cb99047475431985029975c58b61adcff
bhoon <toltolyibh@gmail.com>
2022-10-28 23:03:53 +0900

    MemberEntity 수정

--- src/main/java/com/example/myrestapi/entity/MemberEntity.java
+++ src/main/java/com/example/myrestapi/entity/MemberEntity.java
@@ -1,22 +1,32 @@
 package com.example.myrestapi.entity;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+import javax.persistence.*;
+
+@Table(name = "member")
 @Getter
 @Entity
 public class MemberEntity {
     @Id
-    @Column(name = "member_id")
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "member_id", unique = true)
+    private String memberId;
     private String password;
     private String name;
     private String email;
+
+    @Builder
+    public MemberEntity(Long id, String memberId, String password, String name, String email) {
+        this.id = id;
+        this.memberId = memberId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+    public MemberEntity() {
+
+    }
 }
